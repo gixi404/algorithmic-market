fca04dedf043e10a537ecc7f13349645d99f7397
gioliotta <gixipixel@gmail.com>
2023-08-08 22:30:16 -0300

    mipioja

--- src/components/PayLayout/PayLayout.jsx
+++ src/components/PayLayout/PayLayout.jsx
@@ -1,26 +1,20 @@
-import { styled } from "styled-components"
-import { useState, useEffect } from "react"
-function PayLayout(){
-    const [ buyUrl, setBuyUrl ] = useState("")
-    const handleBuy = async()=>{
-       const res = await fetch("http://localhost:3001/create-checkout-session",{
-            method:"POST"
-        })
-        const data = await res.json()
-        setBuyUrl(data)
+import { styled } from "styled-components";
+import { useState, useEffect } from "react";
+function PayLayout() {
+  const [buyUrl, setBuyUrl] = useState("");
+  const handleBuy = async () => {
+    const res = await fetch("http://localhost:3001/create-checkout-session", {
+      method: "POST",
+    });
+    const data = await res.json();
+    setBuyUrl(data);
+  };
+  useEffect(() => {
+    if (buyUrl) {
+      window.location.href = buyUrl;
     }
-    useEffect(() => {
-        if (buyUrl) {
-            window.location.href= buyUrl
-        }
-      }, [buyUrl]);
-    return(
-        <Button onClick={handleBuy}>
-            Buy
-        </Button>
-    )
+  }, [buyUrl]);
+  return <Button onClick={handleBuy}>Buy</Button>;
 }
-export default PayLayout
-const Button = styled.button`
-
-`;
+export default PayLayout;
+const Button = styled.button``;
