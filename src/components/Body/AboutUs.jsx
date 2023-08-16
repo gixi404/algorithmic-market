c3b766ab482ba886b371c9223bb70b9885cec6e8
Vitto <lexymdz@gmail.com>
2023-08-16 14:05:16 -0300

    si

--- src/components/Body/AboutUs.jsx
+++ src/components/Body/AboutUs.jsx
@@ -3,16 +3,8 @@ import styled from "styled-components";
 function AboutUs() {
   return (
     <AboutUsContainer id="aboutUs">
-      <h4>About us?</h4>
-      <P>
-        At Academia Forex, we offer the best online learning platform
-        because students are always the spirit that mobilizes us. It is because of
-        that the learning program is easily adapted and customized
-        to meet the needs of every student, no matter where
-        neither their level of knowledge is found. With a passionate team,
-        We have assistance by phone, email or video sessions so that
-        learn this skill and become a professional trader.
-      </P>
+      <Text></Text>
+      <Img></Img>
     </AboutUsContainer>
   );
 }
@@ -31,6 +23,9 @@ const AboutUsContainer = styled.div`
   row-gap: 2rem;
   text-align: center;
 `;
-const P = styled.p`
-  width: 63vw;
+const Text = styled.div`
+
+`
+const Img = styled.div`
+
 `;
