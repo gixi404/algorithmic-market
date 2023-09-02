83d5a323b8f51486ca07bef4560a46131b4de325
gixi <gixipixel@gmail.com>
2023-09-02 12:09:52 -0700

    que

--- server/index.js
+++ server/index.js
@@ -1,29 +1,17 @@
-import express from 'express'
-import path from 'path'
-import { config } from 'dotenv'
-config()
-import PayRoute from './routes/pay.routes.js'
-import cors from 'cors'
-import express from 'express'
-import path from 'path'
-import { config } from 'dotenv'
-import PayRoute from './routes/pay.routes.js'
-import cors from 'cors'
-config()
+import express from "express";
+import path from "path";
+import PayRoute from "./routes/pay.routes.js";
+import cors from "cors";
+import { config } from "dotenv";
 
-const PORT = 3001
-export const app = express()
+config();
+const PORT = 3001;
+export const app = express();
 
-const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(express.static(path.resolve("src/components")));
+app.use(PayRoute);
 
-app.use(cors({ origin: 'http://localhost:3000' }))
-app.use(express.json())
-app.use(express.static(path.resolve('src/components')))
-app.use(PayRoute)
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: 'http://localhost:3000' }))
-app.use(express.json())
-app.use(express.static(path.resolve('src/components')))
-app.use(PayRoute)
-
-app.listen(PORT, console.log('running on port', PORT))
+app.listen(PORT, console.log("running on port", PORT));
