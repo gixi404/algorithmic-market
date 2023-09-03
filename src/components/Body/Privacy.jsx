a0f92f0fb6992c2e51935288c33fb8230020bcb4
gixi <gixipixel@gmail.com>
2023-09-03 12:26:38 -0700

    y ahora

--- src/components/Body/Privacy.jsx
+++ src/components/Body/Privacy.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
 import Header from "./Header";
 import Footer from "./Footer";
+import styled from "styled-components";
 
 function Privacy() {
   return (
