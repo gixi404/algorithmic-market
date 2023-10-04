e41961888a3b9c62a5b103f8b90d505ae413edde
gixi <104705119+gioliotta@users.noreply.github.com>
2023-10-04 06:22:22 -0700

    cambios de gixi y arregle la cagadita que te mandaste

--- src/hooks/useBuyLogic.js
+++ src/hooks/useBuyLogic.js
@@ -1,52 +1,55 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-const {user} = useAuth0()
-export function useBuyPetition ({ courses }) {
-  const [buyUrl, setBuyUrl] = useState('')
-  const mappedList = courses.map(course=>
-    ({
+
+export function useBuyPetition(props) {
+  const { courses } = props,
+    [buyUrl, setBuyUrl] = useState(""),
+    { user } = useAuth0();
+
+  useEffect(() => {
+    handleBuy();
+  }, [courses]);
+
+  const mappedList = courses.map(course => ({
       price_data: {
         product_data: {
-          name:course.name,
-          description: course.description
+          name: course.name,
+          description: course.description,
         },
-        unit_amount: (course.cash *100),
+        unit_amount: course.cash * 100,
         currency: "usd",
       },
-      quantity: 1}))
-  
-  const mappedId = courses.map(course => ({id : course.id}))
-  
-  const User = user.email
+      quantity: 1,
+    })),
+    mappedId = courses.map(course => ({ id: course.id })),
+    User = user.email,
+    infoBuy = { list: mappedList, id: mappedId, user: User };
 
-  const infoBuy = {list: mappedList, id: mappedId, user:User}
-  
   async function handleBuy() {
-        const dataToFetch = {
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(infoBuy)
-        }
-        
-        try {
-          const res = await fetch(
-            "http://localhost:3001/create-checkout-session",
-            dataToFetch
-            )
-            const data = await res.json()
+    const dataToFetch = {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(infoBuy),
+    };
+
+    try {
+      const res = await fetch(
+        "http://localhost:3001/create-checkout-session",
+        dataToFetch
+      );
+      const data = await res.json();
 
       const { sessionUrl } = data;
 
       setBuyUrl(sessionUrl);
 
-            localStorage.setItem('id', JSON.stringify(mappedId))
-
-          } catch (error) {
-            console.log('La solicitud de compra falló.')
-          }
+      localStorage.setItem("id", JSON.stringify(mappedId));
+    } catch (error) {
+      console.log("La solicitud de compra falló.");
+    }
   }
-  useEffect(() => {handleBuy()},[courses])
+
   return { buyUrl };
 }
