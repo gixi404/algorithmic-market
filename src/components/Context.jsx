036934048f59ae0bdc10aec1efad3875f2713c81
gixi <104705119+gioliotta@users.noreply.github.com>
2023-10-04 08:11:25 -0700

    uy

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -10,7 +10,7 @@ function Context({ children }) {
     [allCourses, setAllCourses] = useState([
       {
         id: 0,
-        isBought: true,
+        isBought: false,
         name: "Curso Inicial",
         cash: 300,
         description:
