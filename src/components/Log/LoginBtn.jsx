ca8cc2bcde82ea71564a151d4704757b21dced50
gixi <104705119+gioliotta@users.noreply.github.com>
2023-10-04 06:13:51 -0700

    Quité IS_MOBILE y agregé 2 nuevos componentes

--- src/components/Log/LoginBtn.jsx
+++ src/components/Log/LoginBtn.jsx
@@ -1,10 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMyContext } from "../Context";
 import styled from "styled-components";
 
 function LoginBtn() {
-  const { loginWithPopup } = useAuth0(),
-    { IS_MOBILE } = useMyContext();
+  const { loginWithPopup } = useAuth0();
 
   const bodyCore = {
       username: "bakflip",
@@ -28,45 +26,32 @@ function LoginBtn() {
     }
   }
 
-  return IS_MOBILE ? (
-    <Container>
-      <LoginButton onClick={logUser}>Iniciar Sesión</LoginButton>
-    </Container>
-  ) : (
-    <LoginButton onClick={logUser}>Iniciar Sesión</LoginButton>
-  );
+  return <LoginButton onClick={logUser}>Iniciar Sesión</LoginButton>;
 }
 
 export default LoginBtn;
 
-const Container = styled.div`
-    width: 100vw;
-    height: 8vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `,
-  LoginButton = styled.button`
-    position: relative;
-    font-family: "Poppins", monospace;
-    padding: 8px 15px;
-    text-align: center;
-    font-size: calc(12px + (24 - 16) * ((100vw - 320px) / (1920 - 320)));
-    color: #ff6700;
-    outline: 2px solid #ff6700;
-    border-radius: 5px;
-    background-color: transparent;
-    transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
-    overflow: hidden;
-    border: none;
+const LoginButton = styled.button`
+  position: relative;
+  font-family: "Poppins", monospace;
+  padding: 8px 15px;
+  text-align: center;
+  font-size: calc(12px + (24 - 16) * ((100vw - 320px) / (1920 - 320)));
+  color: #ff6700;
+  outline: 2px solid #ff6700;
+  border-radius: 5px;
+  background-color: transparent;
+  transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
+  overflow: hidden;
+  border: none;
 
-    &:hover {
-      cursor: pointer;
-      color: #fff;
-      scale: 1.1;
-      box-shadow: 0 0px 20px rgba(193, 163, 98, 0.4);
-      outline: none;
-      border: none;
-      background-color: #ff6700;
-    }
-  `;
+  &:hover {
+    cursor: pointer;
+    color: #fff;
+    scale: 1.1;
+    box-shadow: 0 0px 20px rgba(193, 163, 98, 0.4);
+    outline: none;
+    border: none;
+    background-color: #ff6700;
+  }
+`;
