09b44eab5ca75b932d791d2fa50b88f92c1bf73a
Vitto <lexymdz@gmail.com>
2023-11-01 17:53:59 -0300

    sexo underground

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import MediaPlayer from "./components/MediaPlayer/MediaPlayer.jsx";
 import Privacy from "./components/Body/Privacy.jsx";
 import IndexCart from "./components/Body/ShoppingCart/IndexCart.jsx";
 import Profile from "./components/Body/Header/Profile.jsx";
+import CoursePurchased from "./components/Courses/CoursePurchased.jsx";
 import "./index.css";
 
 const DOMAIN = "algorithmicmarket.us.auth0.com",
@@ -35,6 +36,7 @@ const DOMAIN = "algorithmicmarket.us.auth0.com",
                 path="/mycourses/:courseid"
                 element={protectedRoute ? <MediaPlayer /> : <Navigate to="/" />}
               />
+              <Route path="/coursepuchased" element={<CoursePurchased />} />
               <Route path="/privacy" element={<Privacy />} />
               <Route path="/shoppingcart" element={<IndexCart />} />
               <Route path="/profile" element={<Profile />} />
